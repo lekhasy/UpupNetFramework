01c8b958671e4c4bfc94dac1350c08f7b577fb58
xmencute <thangphuong@gmail.com>
2017-10-22 23:33:25 +0700

    (+) Giao diện cho danh mục sản phẩm

--- Upup/Controllers/HomeController.cs
+++ Upup/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace Upup.Controllers
     {
         public ActionResult Index()
         {
-            //MailHelper.SendMailNow("lekhasy@outlook.com", "Test", "Test hàng cái nha", null, null, null);
+            //MailHelper.SendMailNow("thangphuong88@gmail.com", "Test", "Test hàng cái nha", null, null, null);
             return View();
         }
 
