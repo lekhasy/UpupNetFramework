7c038e260b2503f5c49b4bac7d5c6ac34f69e42c
xmencute <thangphuong@gmail.com>
2017-10-23 22:44:12 +0700

    (...) Fix send mail function run async

--- Upup/Helpers/MailHelper.cs
+++ Upup/Helpers/MailHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
@@ -15,36 +16,16 @@ namespace Upup.Web.Helpers
         {
             try
             {
-                // Initialize WebMail helper
-                WebMail.SmtpServer = "mail.upup.com.vn";
-                WebMail.SmtpPort = 25;
-                WebMail.UserName = "no-reply@upup.com.vn";
-                WebMail.Password = "Aa6LceC,<3P[3A@)";
-                WebMail.From = "no-reply@upup.com.vn";
-                WebMail.EnableSsl = true;
-
-                // Create array containing file name
-                //var filesList = new string[] { fileAttachment };
-
-                // Attach file and send email
-                // if there is no link to file then send email
-                // without any attachment
-                //if (fileAttachment == null || fileAttachment.IsEmpty())
-                //{
-                ServicePointManager.ServerCertificateValidationCallback =
-                delegate (object s, X509Certificate certificate,
-                         X509Chain chain, SslPolicyErrors sslPolicyErrors)
-                { return true; };
-                WebMail.Send(toEmail, subject, htmlBody, fromEmail, cc, null, true, null, bcc, null, null, null, null);
-                //}
-                //else
-                //{
-                //    // otherwise, send email with file attached
-                //    WebMail.Send(to: customerEmail,
-                //    subject: subjectLine,
-                //    body: "From: " + customerName + "\n\nHis message: " + message,
-                //    filesToAttach: filesList);
-                //}
+                MailMessage mail = new MailMessage("no-reply@upup.com.vn", toEmail);
+                SmtpClient client = new SmtpClient();
+                client.Port = 25;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Host = "mail.upup.com.vn";
+                client.Credentials = new NetworkCredential("no-reply@upup.com.vn", "Aa6LceC,<3P[3A@)");
+                mail.Subject = subject;
+                mail.Body = htmlBody;
+                client.Send(mail);
             }
             catch (Exception ex)
             {
