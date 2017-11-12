76a666f031692265ec9bbe705426dc49135a0c4b
Sy Le <gemini02061994@outlook.com>
2017-11-12 21:25:25 +0700

    Complete dataloading for search

--- Upup/Controllers/SearchController.cs
+++ Upup/Controllers/SearchController.cs
@@ -10,33 +10,55 @@ namespace Upup.Controllers
         // GET: Search
         public async Task<ActionResult> Index(string term)
         {
-            var products = (await Db.SearchForProduct(term)).ToList();
+            var productids = (await Db.SearchForProduct(term)).ToList().Select(p => p.Id);
 
-            var productContainsCode = products.Where(p => p.Code.Contains(term));
+            var products = Db.Products.Where(p => productids.Contains(p.Id)).ToList();
 
-            products.RemoveAll(p => p.Code.Contains(term));
+            var productContainsCode = products.Where(p => p.Code != null && p.Code.Contains(term));
+
+            products.RemoveAll(p => p.Code != null && p.Code.Contains(term));
 
             products = productContainsCode.Concat(products).ToList();
 
-            var postsByTitle = (await Db.SearchForPostByTitle(term)).ToList();
-            var postByContent = (await Db.SearchForPostByContent(term)).ToList();
+            var postsByTitleIds = (await Db.SearchForPostByTitle(term)).ToList().Select(p => p.Id);
+            var postByContentIds = (await Db.SearchForPostByContent(term)).ToList().Select(p => p.Id);
 
+            var postsByTitle = Db.Posts.Where(p => postsByTitleIds.Contains(p.Id)).ToList();
+            var postByContent = Db.Posts.Where(p => postByContentIds.Contains(p.Id)).ToList();
+           
             postByContent.RemoveAll(p => postsByTitle.Any(pt => pt.Id == p.Id));
 
             var posts = postsByTitle.Concat(postByContent);
 
+
+            ViewBag.Term = term;
 
             return View(
                 new SearchModels
                 {
                     SearchTopicResult = posts.Select(p => new SearchItemModels
                     {
-
+                        Id = p.Id,
+                        CategoryId = p.Category.Id,
+                        CategoryName = p.Category.Name,
+                        Content = p.Content,
+                        Title = p.Title,
+                        CategoryName_en = p.Category.Name_en,
+                        Title_en = p.Title_en,
+                        Content_en = p.Content_en
                     }).ToList(),
 
                     SearchProductResult = products.Select(p => new SearchItemModels
                     {
-
+                        Id = p.Id,
+                        Content_en = p.Summary_en,
+                        Content = p.Summary,
+                        Title_en = p.Name_en,
+                        Title = p.Name,
+                        Image = p.ImageUrl,
+                        CategoryId = p.Category.Id,
+                        CategoryName = p.Category.Name,
+                        CategoryName_en = p.Category.Name_en
                     }).ToList()
                 });
         }
--- Upup/ViewModels/SearchItemModels.cs
+++ Upup/ViewModels/SearchItemModels.cs
@@ -8,10 +8,13 @@ namespace Upup.ViewModels
     public class SearchItemModels
     {
         public string Title { get; set; }
+        public string Title_en { get; set; }
         public string Content { get; set; }
+        public string Content_en { get; set; }
         public string Image { get; set; }
         public long Id { get; set; }
         public string CategoryName { get; set; }
+        public string CategoryName_en { get; set; }
         public long CategoryId { get; set; }
     }
 }
