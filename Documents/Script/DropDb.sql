1d79b48d8191b63d63821429a3f7617d55fa1ca1
xmencute <thangphuong@gmail.com>
2017-11-12 15:39:21 +0700

    add script create database

--- Documents/Script/DropDb.sql
+++ Documents/Script/DropDb.sql
@@ -1,13 +1,17 @@
 IF OBJECT_ID('dbo.__MigrationHistory', 'U') IS NOT NULL 
 DROP TABLE dbo.__MigrationHistory;
+IF OBJECT_ID('dbo.PurchaseOrders', 'U') IS NOT NULL 
+DROP TABLE dbo.PurchaseOrders;
+IF OBJECT_ID('dbo.PurchaseOrderDetails', 'U') IS NOT NULL 
+DROP TABLE dbo.PurchaseOrderDetails;
 IF OBJECT_ID('dbo.ProductShipDateApplies', 'U') IS NOT NULL 
 DROP TABLE dbo.ProductShipDateApplies;
 IF OBJECT_ID('dbo.ShipDateSettings', 'U') IS NOT NULL 
 DROP TABLE dbo.ShipDateSettings;
 IF OBJECT_ID('dbo.ProductVariants', 'U') IS NOT NULL 
 DROP TABLE dbo.ProductVariants;
-IF OBJECT_ID('dbo.PurchaseOrderDetails', 'U') IS NOT NULL 
-DROP TABLE dbo.PurchaseOrderDetails;
+IF OBJECT_ID('dbo.ProductVariantUnit', 'U') IS NOT NULL 
+DROP TABLE dbo.ProductVariantUnit;
 IF OBJECT_ID('dbo.Products', 'U') IS NOT NULL 
 DROP TABLE dbo.Products;
 IF OBJECT_ID('dbo.Posts', 'U') IS NOT NULL 
@@ -24,8 +28,6 @@ IF OBJECT_ID('dbo.AspNetUserClaims', 'U') IS NOT NULL
 DROP TABLE dbo.AspNetUserClaims;
 IF OBJECT_ID('dbo.AspNetRoles', 'U') IS NOT NULL 
 DROP TABLE dbo.AspNetRoles;
-IF OBJECT_ID('dbo.PurchaseOrders', 'U') IS NOT NULL 
-DROP TABLE dbo.PurchaseOrders;
 IF OBJECT_ID('dbo.AspNetUsers', 'U') IS NOT NULL 
 DROP TABLE dbo.AspNetUsers;
 IF OBJECT_ID('dbo.AppConfigs', 'U') IS NOT NULL 
