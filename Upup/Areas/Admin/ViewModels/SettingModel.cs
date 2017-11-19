bf79b0fdeae574da88567cb6f998d4c2a10e7b5b
Phuong Quoc Thang <thangphuong88@gmail.com>
2017-11-19 16:22:53 +0700

    code manage setting

--- Upup/Areas/Admin/ViewModels/SettingModel.cs
+++ Upup/Areas/Admin/ViewModels/SettingModel.cs
@@ -8,6 +8,21 @@
         public string Twitter { get; set; }
         public string YoutubeLink { get; set; }
         public string CompanyName { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string WorkTime { get; set; }
         public string Slogan { get; set; }
+        public string Slogan_en { get; set; }
+        public string MetaKeyword { get; set; }
+        public string MetaKeyword_en { get; set; }
+        public string MetaDescription { get; set; }
+        public string MetaDescription_en { get; set; }
+        public string TechBanner { get; set; }
+        public string TechSlogan { get; set; }
+        public string GuideBanner { get; set; }
+        public string GuideSlogan { get; set; }
+        public string EventBanner { get; set; }
+        public string EventSlogan { get; set; }
     }
 }
