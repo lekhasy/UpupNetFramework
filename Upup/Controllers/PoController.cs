5237ea734f63dd6dd2ce50f07857716ed36a009d
Phuong Quoc Thang <thangphuong88@gmail.com>
2017-11-23 22:37:27 +0700

    Modify quote function

--- Upup/Controllers/PoController.cs
+++ Upup/Controllers/PoController.cs
@@ -1,15 +1,11 @@
 ﻿using Microsoft.AspNet.Identity;
-using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
 using Upup.Models;
 using Upup.Utils;
-using Upup.ViewModels;
+using System.IO;
+using System;
 
 namespace Upup.Controllers
 {
@@ -56,6 +52,61 @@ namespace Upup.Controllers
             return RedirectToAction("Index");
         }
 
+        private string CreateEmailQuoteBody(string userName, string code, string title, string message)
+        {
+            string body = string.Empty;
+            var userId = User.Identity.GetUserId();
+            var user = Db.Customers.Find(userId);
+            var carts = user.ProductCarts.ToList();
+            if (carts != null)
+            {
+                var html = string.Empty;
+                var countProduct = 0;
+                foreach (var product in carts)
+                {
+                    countProduct++;
+                    html += "<tr>";
+                    html += "<td rowspan = '3' style='width:5%;text-align:center;'> " + countProduct + " </td>";
+                    html += "<td style ='width:38%'> TÊN SẢN PHẨM</td>";
+                    html += "<td colspan = '2' style = 'width:57%; text-align:right'> Nhãn hiệu </td>";
+                    html += "</tr>";
+                    html += "<tr>";
+                    html += "<td style = 'width:38%' > Mã sản phẩm</td>";
+                    html += "<td colspan = '2' style = 'width:57%; text-align:center' > Thời gian giao hàng </td>";
+                    html += "</tr>";
+                    html += "<tr>";
+                    html += "<td style = 'width:30%; text-align:center' > Đơn giá(vnd) </td>";
+                    html += "<td style = 'width:20%; text-align:center' > Số lượng </td>";
+                    html += "<td style = 'width:45%; text-align:right' > Tổng tiền(vnd) </td>";
+                    html += "</tr > ";
+                }
+            }
+            using (StreamReader reader = new StreamReader(Server.MapPath("~/EmailTemplates/QuotesTemplate.html")))
+            {
+                body = reader.ReadToEnd();
+            }
+            body = body.Replace("[QuoteCode]", "123456");
+            body = body.Replace("[QuoteDate]", DateTime.Now.ToShortDateString());
+            body = body.Replace("[Barcode]", "0987654321");
+            body = body.Replace("[PORef]", "123456");
+            body = body.Replace("[CustomerCode]", DateTime.Now.ToShortDateString());
+            body = body.Replace("[CustomerName]", "0987654321");
+            body = body.Replace("[CompanyPhone]", "123456");
+            body = body.Replace("[TaxNo]", DateTime.Now.ToShortDateString());
+            body = body.Replace("[CompanyAddress]", "0987654321");
+            body = body.Replace("[ContactName]", "123456");
+            body = body.Replace("[Email]", DateTime.Now.ToShortDateString());
+            body = body.Replace("[Phone]", "0987654321");
+            body = body.Replace("[Address]", "0987654321");
+            body = body.Replace("[PayBeforeShip]", "0987654321");
+            body = body.Replace("[PayAfterShip]", "0987654321");
+            body = body.Replace("[Amount]", "0987654321");
+            body = body.Replace("[VAT]", "0987654321");
+            body = body.Replace("[DeliveryFee]", "0987654321");
+            body = body.Replace("[TotalAmount]", "0987654321");
+            body = body.Replace("[HtmlItemInGrid]", "0987654321");
+            return body;
+        }
 
         private PurchaseOrder CreatePO(string code, string name, bool isTemp)
         {
