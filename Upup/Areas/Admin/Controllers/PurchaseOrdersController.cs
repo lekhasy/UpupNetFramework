437c5786016427ba32a555f61d150e0fa76fa680
Phuong Quoc Thang <thangphuong88@gmail.com>
2017-12-01 21:42:57 +0700

    CRUD for PO

---
+++ Upup/Areas/Admin/Controllers/PurchaseOrdersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Upup.Models;
+
+namespace Upup.Areas.Admin.Controllers
+{
+    public class PurchaseOrdersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/PurchaseOrders
+        public ActionResult Index()
+        {
+            return View(db.PurchaseOrders.ToList());
+        }
+
+        // GET: Admin/PurchaseOrders/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PurchaseOrder purchaseOrder = db.PurchaseOrders.Find(id);
+            if (purchaseOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchaseOrder);
+        }
+
+        // GET: Admin/PurchaseOrders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/PurchaseOrders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Code,Name,State")] PurchaseOrder purchaseOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PurchaseOrders.Add(purchaseOrder);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(purchaseOrder);
+        }
+
+        // GET: Admin/PurchaseOrders/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PurchaseOrder purchaseOrder = db.PurchaseOrders.Find(id);
+            if (purchaseOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchaseOrder);
+        }
+
+        // POST: Admin/PurchaseOrders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Code,Name,State")] PurchaseOrder purchaseOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(purchaseOrder).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(purchaseOrder);
+        }
+
+        // GET: Admin/PurchaseOrders/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PurchaseOrder purchaseOrder = db.PurchaseOrders.Find(id);
+            if (purchaseOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchaseOrder);
+        }
+
+        // POST: Admin/PurchaseOrders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            PurchaseOrder purchaseOrder = db.PurchaseOrders.Find(id);
+            db.PurchaseOrders.Remove(purchaseOrder);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
