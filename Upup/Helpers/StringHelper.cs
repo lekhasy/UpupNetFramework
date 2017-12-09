86fe986d35188a6ceb47fd64644447ff9719d743
Phuong Quoc Thang <thangphuong88@gmail.com>
2017-12-09 16:18:00 +0700

    Include barcode genarator

--- Upup/Helpers/StringHelper.cs
+++ Upup/Helpers/StringHelper.cs
@@ -1,4 +1,9 @@
-﻿namespace Upup.Helpers
+﻿using QRCoder;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace Upup.Helpers
 {
     public static class StringHelper
     {
@@ -62,6 +67,23 @@
                     }
             }
             return stateString;
+        }
+
+        public static string CreateQrCode(string code)
+        {
+            var imageUrl = string.Empty;
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeGenerator.QRCode qrCode = qrGenerator.CreateQrCode(code, QRCodeGenerator.ECCLevel.Q);
+            using (Bitmap bitMap = qrCode.GetGraphic(20))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    byte[] byteImage = ms.ToArray();
+                    imageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+                }
+                return imageUrl;
+            }
         }
     }
 }
